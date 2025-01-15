e2c7b073209c2a5cb94a899035a30fd03b560ebf
qwertyabu <monkey.famil4@gmail.com>
2025-01-15 14:01:00 +0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,25 @@
+let abu = prompt('String typedagi malumot kiriting')
+alert(abu)
+
+
+let admin = prompt('Number typedagi malumot kiriting');
+alert(admin)
+console.log(admin);
+
+let two = prompt('boolean typedagi malumot kiriting');
+alert(two)
+console.log(two);
+
+let one = confirm('hamma malumotlarni togri kiritganingizni ishonasmi');
+alert(one)
+console.log(one);
+
+let four = (`siz kiritgan ${abu} sozi string typega tegishli
+siz kiritgan ${admin} sozi string typega tegishli
+siz kiritgan ${two} sozi string typega tegishli
+`);
+    alert(four)
+
+
+
+
